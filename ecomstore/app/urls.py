11712059ab0fe9734c5ddbdf1b450749fe0318e9
mohammed-7 <khanhuzaif@live.com>
2023-10-11 13:40:51 +0530

    added new files for header , and include tag too.

--- ecomstore/app/urls.py
+++ ecomstore/app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('departments/', views.departments),
+    path('services/', views.services),
+
+
 ]
--- ecomstore/app/views.py
+++ ecomstore/app/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "app/index.html")
+
+
+def departments(request):
+    return render(request, "app/departments.html")
+
+
+def services(request):
+    return render(request, "app/services.html")
